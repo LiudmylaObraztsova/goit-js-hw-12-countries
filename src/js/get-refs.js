382a8e0078844884aea36159c3bcb235264d9675
LiudmylaObraztsova <lpfoneone@gmail.com>
2021-06-13 14:42:44 +0300

    Удаляет все файлы

--- src/js/get-refs.js
+++
@@ -1,6 +0,0 @@
-export default function getRefs() {
-    return {
-        cardContainer: document.querySelector('.list__container'),
-        searchForm: document.querySelector('.search-form')
-    }
-}
